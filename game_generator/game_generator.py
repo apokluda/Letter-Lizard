18c6dab916245db293d82ef5394f962a7d6d3d4e
Alexander Pokluda <apokluda@gmail.com>
2014-01-27 21:54:27 -0500

    game_generator.py now counts frequency of all letters in dictionary

--- game_generator/game_generator.py
+++ game_generator/game_generator.py
@@ -37,20 +37,62 @@ import fileinput
 #   - canadian-words.95
 
 parser = argparse.ArgumentParser()
-parser.add_argument("difficulty", type=str, choices=["easy", "medium", "hard", "insane"], default="easy", nargs="?", help="level of difficulty")
-parser.add_argument("-s", "--size", type=int, default=6, help="the number of scrambled letters in the game")
-parser.add_argument("-n", "--num", type=int, default=1, help="the number of games to generate")
+parser.add_argument(
+        "difficulty", 
+        type=str,
+        choices=["easy", "medium", "hard", "insane"],
+        default="easy",
+        nargs="?",
+        help="level of difficulty")
+parser.add_argument(
+        "-s", "--size",
+        type=int,
+        default=6,
+        help="the number of scrambled letters in the game")
+parser.add_argument(
+        "-n", "--num",
+        type=int,
+        default=1,
+        help="the number of games to generate")
 args = parser.parse_args()
 
 dd = "scowl-7.1/final/"
 
-dicts = {'easy': [dd+"english-words.10", dd+"english-words.20", dd+"english-words.35", dd+"canadian-words.10", dd+"canadian-words.20", dd+"canadian-words.35"]}
-dicts['medium'] = [dd+"english-words.40", dd+"english-words.50", dd+"canadian-words.40", dd+"canadian-words.50"] + dicts['easy']
-dicts['hard'] = [dd+"english-words.55", dd+"english-words.60", dd+"english-words.70", dd+"canadian-words.55", dd+"canadian-words.60", dd+"canadian-words.70"] + dicts['medium']
-dicts['insane'] = [dd+"english-words.80", dd+"english-words.95", dd+"canadian-words.80", dd+"canadian-words.95"] + dicts['hard']
+dicts = {'easy': [dd+"english-words.10", dd+"english-words.20", dd+"english-words.35",
+        dd+"canadian-words.10", dd+"canadian-words.20", dd+"canadian-words.35"]}
+dicts['medium'] = [dd+"english-words.40", dd+"english-words.50", 
+        dd+"canadian-words.40", dd+"canadian-words.50"] + dicts['easy']
+dicts['hard'] = [dd+"english-words.55", dd+"english-words.60", dd+"english-words.70",
+        dd+"canadian-words.55", dd+"canadian-words.60", dd+"canadian-words.70"] + dicts['medium']
+dicts['insane'] = [dd+"english-words.80", dd+"english-words.95",
+        dd+"canadian-words.80", dd+"canadian-words.95"] + dicts['hard']
 
-count = 0
-with fileinput.input(files=dicts[args.difficulty]) as f:
+class LetterCounter:
+    letter_counts = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0,
+    'J':0, 'K':0, 'L':0, 'M':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0,
+    'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
+    total_letters = 0
+    
+    def count_word(self, word):
+        for c in word.upper():
+            try:
+                self.letter_counts[c] += 1
+                self.total_letters += 1
+            except KeyError:
+                pass
+
+    def letter_frequency(self):
+        letter_frequencies = {}
+        for k, v in self.letter_counts.items():
+            letter_frequencies[k] = v / self.total_letters
+        return letter_frequencies
+
+lc = LetterCounter()
+with fileinput.input(files=dicts[args.difficulty],openhook=fileinput.hook_encoded("iso-8859-1")) as f:
     for line in f:
-        count += 1
-print(count)
+        lc.count_word(line)
+        
+print(lc.letter_counts)
+letter_frequency = lc.letter_frequency()
+for k in sorted(lc.letter_frequency().keys()):
+    print("'{}': {},".format(k, letter_frequency[k]))
