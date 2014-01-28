b5dc77bf6b32b0bdc59c7976b138abd19af6bc33
wexlermi <wexlermichaeli@gmail.com>
2014-01-28 00:31:37 -0500

    guess words correctly, appear on right hand side, amongst other changes

--- letter_lizard.py
+++ letter_lizard.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Import a library of functions called 'pygame'
 import pygame
 import random
@@ -7,6 +8,7 @@ pygame.init()
 
 black = [ 0, 0, 0]
 white = [255,255,255]
+sky_blue = [  66,  136, 227]
 
 # Set the height and width of the screen
 size = [1000,600]
@@ -19,9 +21,13 @@ clock = pygame.time.Clock()
 done = False
 
 puzzle_letters = ['a', 'b', 'g', 'e', 'c', 'r', 'l', 's']
+solutions = ['bag', 'bear', 'sear', 'lag', 'car']
 square_width = 50
 left_margin = 20
 spacing = 30
+
+correct_words_left = 800
+correct_words_top = 100
 
 puzzle_letter_font = pygame.font.SysFont("Arial", 30, bold=True)
 guessed_letter_font = pygame.font.SysFont("Arial", 20, bold=True)
@@ -33,6 +39,8 @@ letters_guessed_left_margin = 20
 letters_guessed_top_margin = 300
 letters_guessed = []
 
+words_guessed_correct = []
+
 while done == False:
 
 	# ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
@@ -42,7 +50,20 @@ while done == False:
 			# User pressed down on a key
 		
 		elif event.type == pygame.KEYDOWN:
-			if (event.key in range(256)):
+			if (event.key == pygame.K_RETURN):
+				guessed_word = ''.join(letters_guessed)
+				if ( guessed_word in solutions):
+					words_guessed_correct.append(guessed_word)
+					letters_guessed = []
+				else:
+					print "wrong"
+				#print "hello"
+			elif (event.key == pygame.K_BACKSPACE):
+				if (len(letters_guessed) >= 1):
+					del letters_guessed[len(letters_guessed) - 1]
+			elif (event.key == pygame.K_SPACE):
+				random.shuffle(puzzle_letters)
+			elif (event.key in range(256)):
 				letter = chr(event.key)
 				letters_guessed.append(letter)
 				num_letters_pressed += 1
@@ -60,7 +81,7 @@ while done == False:
 	
 		
     # Set the screen background
-	screen.fill(white)
+	screen.fill(sky_blue)
 	
 	for i in range(len(letters_guessed)):
 		letter = letters_guessed[i]
@@ -78,6 +99,11 @@ while done == False:
 		pygame.draw.rect(screen, black, (x, y ,square_width,square_width), 1)
 		screen.blit(letter_label, (x + square_width/4, y + square_width/5))
 		
+	for i in range(len(words_guessed_correct)):
+		word = words_guessed_correct[i]
+		word_label = puzzle_letter_font.render(word, 1, black)
+		#pygame.draw.rect(screen, black, (correct_words_left, correct_words_top ,square_width,square_width), 1)
+		screen.blit(word_label, (correct_words_left, correct_words_top + i*square_width))
     # Go ahead and update the screen with what we've drawn.
 	pygame.display.flip()
 	clock.tick(20)
